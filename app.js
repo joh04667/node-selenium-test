0e9d164fbe5fab7b6caf711e03666c87d7a5bd51
joh04667 <joh04667@gmail.com>
2016-09-28 11:55:05 -0500

    adds env variables, server actually works

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-var selenium = require('selenium-webdriver');
+// utility module for environment vars
+require('dotenv').config();
+
+// Import Selenium instance and utility functions
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+// Import browser drivers
 var chrome = require('chromedriver')
-var driver = new selenium.Builder()
-  // .withCapabilities(selenium.Capabilities.chrome())
-  .forBrowser('Chrome')
-  .usingServer('https://jodev04.praeses.com/')
+
+
+
+
+
+var driver = new webdriver.Builder()
+  // .withCapabilities(webdriver.Capabilities.chrome())
+  .forBrowser('chrome')
   .build();
 
-  driver.get('https://jodev04.praeses.com/')
+
+driver.get(process.env.TEST_URL)
+driver.wait(until.titleIs('webdriver - Google Search'), 10000)
